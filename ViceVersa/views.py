e72bdb7ff8c4b7f567f02ee0c5ee90019dc7a1b9
Mykyta <nikita.ua1311@gmail.com>
2023-03-06 00:50:24 +0200

    add count number

--- ViceVersa/views.py
+++ ViceVersa/views.py
@@ -9,5 +9,6 @@ def home(request):
 
 def reverse(request):
     user_text = request.GET['usertext']
+    count_num = len(user_text.split(" "))
     reverse_text = user_text[::-1]
-    return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_text})
+    return render(request, 'reverse.html', {'usertext': user_text, 'reversetext': reverse_text, 'numcount': count_num})
